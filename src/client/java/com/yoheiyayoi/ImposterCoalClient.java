2226faee83db8623f73d8df596c462ee12d45547
yohei_yayoi <93367004+yoheiyayoi@users.noreply.github.com>
2026-03-12 13:18:21 +0700

    Initial commit of Fabric mod project

---
+++ src/client/java/com/yoheiyayoi/ImposterCoalClient.java
@@ -0,0 +1,10 @@
+package com.yoheiyayoi;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ImposterCoalClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
