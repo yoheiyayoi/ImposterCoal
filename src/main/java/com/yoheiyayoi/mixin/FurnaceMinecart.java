4b23efd593b80b59075e0362644fe142c7c8dca7
yohei_yayoi <93367004+yoheiyayoi@users.noreply.github.com>
2026-03-14 00:59:33 +0700

    Update FurnaceMinecart Mixin
    Add overheat system for furnace cart

--- src/main/java/com/yoheiyayoi/mixin/FurnaceMinecart.java
+++ src/main/java/com/yoheiyayoi/mixin/FurnaceMinecart.java
@@ -2,12 +2,20 @@ package com.yoheiyayoi.mixin;
 
 import com.yoheiyayoi.ImposterCoal;
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.vehicle.minecart.MinecartFurnace;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
@@ -26,39 +34,86 @@ public class FurnaceMinecart {
     //-- Local
     private int currentFuel = 0;
     private int MAX_FUEL_TO_PUSH = 10;
-    private boolean isOverheat = false;
 
-    private int COAL_COOLDOWN = 30; // 1.5s
-    private int CHARCOAL_COOLDOWN = 60; // 3s
+    private int COAL_COOLDOWN = 0; // 1.5s
+    private int CHARCOAL_COOLDOWN = 20 * 3; // 3s
+    private int AFTER_FIRE_COOLDOWN = 20 * 20; // 20s
     private int fuelCooldown = 0;
 
     //-- Functions
 
+    // react useState but minecraft edition
+    @Unique
+    private static final EntityDataAccessor<Boolean> IS_OVERHEAT = SynchedEntityData.defineId(
+            MinecartFurnace.class,
+            EntityDataSerializers.BOOLEAN
+    );
+
+    @Inject(method = "defineSynchedData", at = @At("TAIL"))
+    private void addSynchedData(SynchedEntityData.Builder builder, CallbackInfo ci) {
+        builder.define(IS_OVERHEAT, false);
+    }
+
+    @Unique
+    private boolean getIsOverheat() {
+        return ((MinecartFurnace)(Object) this).getEntityData().get(IS_OVERHEAT);
+    }
+
+    @Unique
+    private void setIsOverheat(boolean value) {
+        ((MinecartFurnace)(Object) this).getEntityData().set(IS_OVERHEAT, value);
+    }
+
     // custom fuel method
     @Inject(method = "interact", at = @At("HEAD"), cancellable = true)
-    private void addFuel(Player player, InteractionHand interactionHand, CallbackInfoReturnable<InteractionResult> cir) {
+    private void onInteract(Player player, InteractionHand interactionHand, CallbackInfoReturnable<InteractionResult> cir) {
+        MinecartFurnace cart = (MinecartFurnace)(Object) this;
         ItemStack stack = player.getItemInHand(interactionHand);
+        Item item = stack.getItem();
 
-        if (fuelCooldown > 0) {
+        // dub fire
+        if (getIsOverheat() && item == Items.WATER_BUCKET) {
+            setIsOverheat(false);
+            cart.clearFire();
+            fuelCooldown = AFTER_FIRE_COOLDOWN;
+
+            if (!player.isCreative()) {
+                player.setItemInHand(interactionHand, new ItemStack(Items.BUCKET));
+            }
+
+            // play sound on server
+            if (!cart.level().isClientSide()) {
+                ServerLevel serverLevel = (ServerLevel) cart.level();
+                serverLevel.playSound(
+                        null,
+                        cart.blockPosition(),
+                        SoundEvents.BUCKET_EMPTY,
+                        SoundSource.BLOCKS,
+                        1.0f,
+                        1.0f
+                );
+            }
+
+            cir.setReturnValue(InteractionResult.SUCCESS);
+            return;
+        }
+
+        // cannot add fuel at the moment
+        if (fuelCooldown > 0 || getIsOverheat()) {
             cir.setReturnValue(InteractionResult.FAIL);
             return;
         }
 
-        if (stack.getItem() == Items.COAL || stack.getItem() == Items.CHARCOAL) {
+        // add fuel
+        if (item == Items.COAL || item == Items.CHARCOAL) {
             currentFuel += 1;
 
             // set cooldown
             if (currentFuel != MAX_FUEL_TO_PUSH) {
-                if (stack.getItem() == Items.COAL) {
-                    fuelCooldown = COAL_COOLDOWN;
-                    ImposterCoal.LOGGER.info(String.valueOf(fuelCooldown));
-                } else {
-                    fuelCooldown = CHARCOAL_COOLDOWN;
-                }
+                fuelCooldown = item == Items.COAL ? COAL_COOLDOWN : CHARCOAL_COOLDOWN;
             }
 
             pushCartIfCan(player);
-            // cart.push = cart.position().subtract(playerPos).horizontal().normalize().scale(0.02);
 
             if (!player.isCreative())
                 stack.consume(1, player);
@@ -70,9 +125,31 @@ public class FurnaceMinecart {
     // Cooldown handler
     @Inject(method = "tick", at = @At("TAIL"))
     private void tickCooldown(CallbackInfo ci) {
-        MinecartFurnace cart = (MinecartFurnace)(Object)this;
+        MinecartFurnace cart = (MinecartFurnace)(Object) this;
+
+        // render fire effect (client)
+        if (getIsOverheat()) {
+            cart.igniteForTicks(1);
+        }
 
         if (cart.level().isClientSide()) return;
+
+        // fire effect (server)
+        if (getIsOverheat()) {
+            ServerLevel serverLevel = (ServerLevel) cart.level();
+
+            // play sound every 2 sec
+            if (cart.tickCount % 40 == 0) {
+                serverLevel.playSound(
+                        null,
+                        cart.blockPosition(),
+                        SoundEvents.FIRE_AMBIENT,
+                        SoundSource.BLOCKS,
+                        2.0f,
+                        1.0f
+                );
+            }
+        }
 
         if (fuelCooldown > 0) {
             fuelCooldown--;
@@ -91,11 +168,24 @@ public class FurnaceMinecart {
 
     @Unique
     private void updateTitle() {
+        MinecartFurnace cart = (MinecartFurnace)(Object) this;
         int red = MAX_FUEL_TO_PUSH - currentFuel;
-        MinecartFurnace cart = (MinecartFurnace)(Object)this;
-        Component title = Component.literal("[").withStyle(ChatFormatting.BOLD, ChatFormatting.GRAY)
-                .append(Component.literal("❙".repeat(currentFuel)).withStyle(ChatFormatting.BOLD, ChatFormatting.GREEN))
-                .append(Component.literal("❙".repeat(red)).withStyle(ChatFormatting.BOLD, ChatFormatting.RED))
+
+        Component content;
+
+        if (getIsOverheat()) {
+            content = Component.literal("เครื่องยนต์ร้อนเกินไป!")
+                    .withStyle(ChatFormatting.BOLD, ChatFormatting.RED);
+        } else {
+            content = Component.literal("❙".repeat(currentFuel))
+                    .withStyle(ChatFormatting.BOLD, ChatFormatting.GREEN)
+                    .append(Component.literal("❙".repeat(red))
+                            .withStyle(ChatFormatting.BOLD, ChatFormatting.RED));
+        }
+
+        Component title = Component.literal("[")
+                .withStyle(ChatFormatting.BOLD, ChatFormatting.GRAY)
+                .append(content)
                 .append(Component.literal("]").withStyle(ChatFormatting.BOLD, ChatFormatting.GRAY));
 
         cart.setCustomName(title);
@@ -104,20 +194,47 @@ public class FurnaceMinecart {
 
     @Unique
     private void pushCartIfCan(@Nullable Player player) {
-        MinecartFurnace cart = (MinecartFurnace)(Object)this;
+        MinecartFurnace cart = (MinecartFurnace)(Object) this;
 
         if (player != null && currentFuel == MAX_FUEL_TO_PUSH) {
-            Vec3 playerPos = player.position();
-            cart.push = cart.position().subtract(playerPos).horizontal().normalize().scale(0.035);
-
             // reset fuel
             currentFuel = 0;
 
-            // รถไฟไฟไหม้ omg
-            // int random = (int)(Math.random() * 4) + 1;
-            // if (random != 1) return;
+            // random overheat
+            if (!cart.level().isClientSide()) {
+                int random = (int) (Math.random() * 4) + 1; // 1 in 4
+                if (random == 2) {
+                    ImposterCoal.LOGGER.info("FIRE");
+                    setIsOverheat(true);
+                };
+            }
 
-            // cart.lavaIgnite();
+            Vec3 playerPos = player.position();
+
+            // cart not move if overheat
+            if (!getIsOverheat()) {
+                cart.push = cart.position().subtract(playerPos).horizontal().normalize().scale(0.035);
+            }
+
+            if (!cart.level().isClientSide()) {
+                // effect
+                ServerLevel serverLevel = (ServerLevel) cart.level();
+                Vec3 pos = cart.position();
+                serverLevel.sendParticles(
+                        ParticleTypes.FLAME,
+                        pos.x, pos.y + 0.5, pos.z,
+                        6, 0.4, 0.4, 0.4, 0.02
+                );
+
+                serverLevel.playSound(
+                        null,
+                        cart.blockPosition(),
+                        SoundEvents.ENDER_EYE_DEATH,
+                        SoundSource.BLOCKS,
+                        1.0f,
+                        1.0f
+                );
+            }
         }
     }
 }
